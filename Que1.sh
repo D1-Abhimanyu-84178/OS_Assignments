a130a8adf36fb99dd48d6d2f80527de2ffbed7b3
abhipatil2001 <abhipatil9798@gmail.com>
2024-06-14 23:10:40 +0530

    Que1_assignment 5 completed

---
+++ Que1.sh
@@ -0,0 +1,19 @@
+#/!bin/bash
+
+#1. Write a shell script to display your LOGIN NAME and HOME directory.
+
+#echo "Hello World"
+#echo "Enter cmd : " 
+#read cmd1
+$cmd1
+
+#echo "enter cmd 2 : "
+#read cmd2
+#$cmd2
+
+
+# Display the login name
+echo "Your login name is": $USER
+
+# Display the home directory
+echo "Your home directory is": $HOME
